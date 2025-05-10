6a2a03fd39ef2c335b25ae96d83f16e23d50de7e
Dauisberto <costos@byupathway.com>
2025-05-09 23:50:04 -0300

    Included Core 1&2 requirements and corrected range of guessing from 1,5 to 1,100

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,18 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Core 1&2 requirements
+        /*
+        Console.WriteLine("Welcome to Number Akinator.");
+        Console.WriteLine("You'll try to guess the magic number");
+        string guesss="";
+        string numbers="";
+        string magic="";
+        int guesss_number=0;
+        int magic_number;
+        Console.Write("What's the magic number? ");
+        magic=Console.ReadLine();
+        magic_number=int.Parse(magic);
+        Console.WriteLine("");
+        Console.Write("What's your guessing? ");
+        guesss=Console.ReadLine();
+        guesss_number=int.Parse(guesss);
+
+        while (guesss_number!=magic_number)
+        {
+            Console.WriteLine("Nope, try again! ");
+            if (guesss_number<magic_number)
+            {
+                Console.WriteLine("Higher!");
+                Console.Write("What's your guessing? ");
+                guesss=Console.ReadLine();
+                guesss_number=int.Parse(guesss);
+            }
+            else if (guesss_number>magic_number)
+            {
+               Console.WriteLine("Lower!");
+                Console.Write("What's your guessing? ");
+                guesss=Console.ReadLine();
+                guesss_number=int.Parse(guesss); 
+            }
+        }
+        Console.WriteLine("Congrats! You guessed the number");
+        */
+
         //Core 3 requirements
         Console.WriteLine("Welcome to Number Reverse Akinator.");
         Console.WriteLine("You must try to guess which number i'm thinking about");
-        Random randomGenerator = new Random();
-        int akinator_number=randomGenerator.Next(1,5);
+        
         int keep_playing=0;
         int count=1;
         string keep ="";
 
         while (keep_playing==0)
         {
-
+        Random randomGenerator = new Random();
+        int akinator_number=randomGenerator.Next(1,5);
             Console.Write("What is your number guess? ");
             string guess = Console.ReadLine();
             int guess_number;
@@ -39,6 +77,7 @@ class Program
             }
 
             Console.WriteLine("Congrats! You guessed it");
+            Console.WriteLine($"You guessed it in {count} rows");
             Console.WriteLine("Would you like you play again? If so, type 0, otherwise, type any number");
             keep=Console.ReadLine();
             keep_playing=int.Parse(keep);
