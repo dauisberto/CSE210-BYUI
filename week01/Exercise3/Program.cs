44464e2ef7cd48ffeb0aa79c7ab3341caf0e73b4
Dauisberto <costos@byupathway.com>
2025-05-09 23:30:58 -0300

    Included core requirement 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Core 3 requirements
+        Console.WriteLine("Welcome to Number Reverse Akinator.");
+        Console.WriteLine("You must try to guess which number i'm thinking about");
+        Random randomGenerator = new Random();
+        int akinator_number=randomGenerator.Next(1,5);
+        int keep_playing=0;
+        int count=1;
+        string keep ="";
+
+        while (keep_playing==0)
+        {
+
+            Console.Write("What is your number guess? ");
+            string guess = Console.ReadLine();
+            int guess_number;
+            guess_number=int.Parse(guess);
+
+            while (guess_number!=akinator_number)
+            {
+                Console.WriteLine("Nope, try again! ");
+                count +=1;
+                if (guess_number<akinator_number)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (guess_number>akinator_number)
+                {
+                    Console.WriteLine("Lower! ");
+                }
+                Console.Write("What is your new guess? ");
+                guess=Console.ReadLine();
+                guess_number=int.Parse(guess);
+            }
+
+            Console.WriteLine("Congrats! You guessed it");
+            Console.WriteLine("Would you like you play again? If so, type 0, otherwise, type any number");
+            keep=Console.ReadLine();
+            keep_playing=int.Parse(keep);
+        }
+
     }
 }
