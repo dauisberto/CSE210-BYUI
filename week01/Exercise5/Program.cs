818f0bbb564ad37d82cb9b4fc80133e733c10e98
Dauisberto <costos@byupathway.com>
2025-05-10 18:44:23 -0300

    Created prompts as requested on Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string user_name = PromptUserName();
+        int user_number = PromptUserNumber();
+        int square_number = SquareNumber(user_number);
+        DisplayResult(user_name,square_number);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to our BYUI App! ");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your Name? ");
+        string name_typing = Console.ReadLine();
+        return name_typing;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        string string_number = Console.ReadLine();
+        int fav_number=int.Parse(string_number);
+        return fav_number;
+    }
+    static int SquareNumber(int numb)
+    {
+        int square = numb*numb;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, your squared number is {square}");
+    }
+        
+    
 }
