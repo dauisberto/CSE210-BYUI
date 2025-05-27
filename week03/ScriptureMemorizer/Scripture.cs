cd267ff6bf471d1fe38d2f2fd1adaf007ec43ba6
Dauisberto <costos@byupathway.com>
2025-05-27 09:23:24 -0300

    1st creation of classes Reference, Scripture and Word

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,13 @@
+public class Scripture
+{
+    Reference _reference;
+    List<Word> _words;
+
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        
+    }
+
+}
