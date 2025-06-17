83bdfb72c432aed5564d36df0d16478d8a4540f8
Dauisberto <costos@byupathway.com>
2025-06-17 01:37:06 -0300

    Created Shape class and its children Circle, Rectangle and Square. All of them printing in Program.cs that was updated too

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square cuadro = new Square("Red", 2.1);
+
+        Console.WriteLine($"{cuadro.GetColor()}");
+        Console.WriteLine($"{cuadro.GetArea()}");
+
+        Console.WriteLine("");
+
+        Rectangle rectangulo = new Rectangle("Blue", 2.21, 2.21);
+        Console.WriteLine($"{rectangulo.GetColor()}");
+        Console.WriteLine($"{rectangulo.GetArea()}");
+
+        Console.WriteLine("");
+
+        Circle circulo = new Circle("Orange", 4);
+        Console.WriteLine($"{circulo.GetColor()}");
+        Console.WriteLine($"{circulo.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(cuadro);
+        shapes.Add(rectangulo);
+        shapes.Add(circulo);
+
+        foreach (Shape s in shapes)
+        {
+            Console.Write($"{s.GetColor()} ");
+            Console.Write($"{s.GetArea()} ");
+        }
+
     }
 }
